4553ce324190a59aa9787509577a48451f0b26d2
Maxim Stolyarchuk <maximstolyarchuk@gmail.com>
2020-07-30 17:28:07 +0200

    Session changes

--- workflow/code/gen_starting_structures.py
+++ workflow/code/gen_starting_structures.py
@@ -1,8 +1,17 @@
-template = """#pivot auto
+import numpy as np
+
+header="""#pivot auto
 #centered receptor: false
 #centered ligands: false
-#1
-           0           0           0           0           0           0
-           0           0           0           {:.3f}    {:.3f}   {:.3f}
 """
-result = template.format(offset["x"], offset["y"], offset["z"])
+
+template = ("0           0           0           0           0           0\n"
+           "{:.3f}           {:.3f}           {:.3f}          {:.3f}    {:.3f}   {:.3f}\n")
+
+np.random.seed(seed)
+
+result = header
+for i in range(nstruc):
+    result += "#{}\n".format(i+1)
+    rot_x, rot_y, rot_z = np.random.uniform(0, 2*np.math.pi, 3)
+    result += template.format(rot_x, rot_y, rot_z, offset["x"], offset["y"], offset["z"])
